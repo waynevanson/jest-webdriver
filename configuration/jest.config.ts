8fb4a0dfcdb404ca7bcc9bc406ccb21e3158c9a8
Wayne Van Son <waynevanson@gmail.com>
2021-08-12 08:36:11 +1000

    tests(config): remove node env

--- configuration/jest.config.ts
+++ configuration/jest.config.ts
@@ -1,6 +1,27 @@
 import { InitialOptionsTsJest } from "ts-jest/dist/types"
+import { defaults as TsJestPreset } from "ts-jest/presets/index"
+import { defaults as WebdriverIOPreset } from "../dist/jest-preset"
+
+const projectDefaults = {
+  rootDir: "./",
+}
 
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  ...TsJestPreset,
+  ...WebdriverIOPreset,
+  rootDir: "../",
+  globals: { webdriverio: {}, "ts-jest": {} },
+  // projects: [
+  //   {
+  //     ...projectDefaults,
+  //     displayName: "test",
+  //   },
+  //   {
+  //     ...projectDefaults,
+  //     displayName: "lint",
+  //     testMatch: ["./src/**/*.ts"],
+  //     runner: "jest-runner-eslint",
+  //   },
+  // ],
+  // watchPlugins: ["jest-runner-eslint/watch-fix"],
 } as InitialOptionsTsJest
