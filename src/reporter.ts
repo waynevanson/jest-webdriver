c09f9d29d460008916f033facaf4d5778eb5ed08
Wayne Van Son <waynevanson@gmail.com>
2021-08-12 08:37:03 +1000

    chore(reporter): comment out a bunch of stuff – not ready for this yet

--- src/reporter.ts
+++ src/reporter.ts
@@ -9,52 +9,44 @@ import type {
 import "webdriverio/async"
 import type { Config } from "@jest/types"
 import type { TestCaseResult } from "@jest/test-result"
-import type { WebdriverIOConfigOptionsJest } from "./types"
+import type { WebdriverIOGlobalOptions } from "./types"
 
-export default class WebdriverIOReporter
-  implements Reporter
-{
+export default class WebdriverIOReporter implements Reporter {
   constructor(
-    config: Config.GlobalConfig,
-    options: WebdriverIOConfigOptionsJest,
+    public config: Config.GlobalConfig,
+    public options: WebdriverIOGlobalOptions,
   ) {}
 
-  readonly onTestResult?: (
-    test: Test,
-    testResult: TestResult,
-    aggregatedResult: AggregatedResult,
-  ) => Promise<void> | void
+  // readonly onTestResult?: (
+  //   test: Test,
+  //   testResult: TestResult,
+  //   aggregatedResult: AggregatedResult,
+  // ) => Promise<void> | void
 
-  readonly onTestFileResult?: (
-    test: Test,
-    testResult: TestResult,
-    aggregatedResult: AggregatedResult,
-  ) => Promise<void> | void
+  // readonly onTestFileResult?: (
+  //   test: Test,
+  //   testResult: TestResult,
+  //   aggregatedResult: AggregatedResult,
+  // ) => Promise<void> | void
 
-  readonly onTestCaseResult?: (
-    test: Test,
-    testCaseResult: TestCaseResult,
-  ) => Promise<void> | void
+  // readonly onTestCaseResult?: (
+  //   test: Test,
+  //   testCaseResult: TestCaseResult,
+  // ) => Promise<void> | void
 
   readonly onRunStart: (
     results: AggregatedResult,
     options: ReporterOnStartOptions,
-  ) => Promise<void> | void
+  ) => Promise<void> | void = async () => {}
 
-  readonly onTestStart?: (
-    test: Test,
-  ) => Promise<void> | void
+  // readonly onTestStart?: (test: Test) => Promise<void> | void
 
-  readonly onTestFileStart?: (
-    test: Test,
-  ) => Promise<void> | void
+  // readonly onTestFileStart?: (test: Test) => Promise<void> | void
 
   readonly onRunComplete: (
     contexts: Set<Context>,
     results: AggregatedResult,
-  ) => Promise<void> | void
+  ) => Promise<void> | void = async () => {}
 
   readonly getLastError: () => Error | void = () => {}
 }
-
-// TYPES
