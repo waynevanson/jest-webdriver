d4df69f3151ea6bdd40ac4d6b3c698c27541af69
Wayne Van Son <waynevanson@gmail.com>
2021-08-14 18:36:47 +1000

    fix: remove jest-preset

--- configuration/jest.config.ts
+++ configuration/jest.config.ts
@@ -1,24 +1,10 @@
 import { InitialOptionsTsJest } from "ts-jest/dist/types"
 import { register } from "ts-node"
-import { defaults as WebdriverIOPreset } from "../src/jest-preset"
 
 register()
 
 const configuration: InitialOptionsTsJest = {
-  ...WebdriverIOPreset,
   rootDir: "../",
-  globals: {
-    webdriverio: {
-      logLevel: "silent",
-      capabilities: {
-        browserName: "chrome",
-        "goog:chromeOptions": { headless: true },
-      },
-      services: [`chromedriver`],
-    },
-    // ts-jest throws when globals is defined but there is not configuration
-    "ts-jest": {},
-  },
 }
 
 export default configuration
