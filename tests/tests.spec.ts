9956678f41a1a333c6c03fd9054b979f22762677
Wayne Van Son <waynevanson@gmail.com>
2021-08-15 08:14:51 +1000

    chore: workspaces

--- tests/tests.spec.ts
+++
@@ -1,9 +0,0 @@
-describe("environment", () => {
-  it.skip("should have a browser object defined", () => {
-    expect(browser).toBeDefined()
-  })
-
-  it.skip("should have a driver object defined", () => {
-    expect(driver).toBeDefined()
-  })
-})
