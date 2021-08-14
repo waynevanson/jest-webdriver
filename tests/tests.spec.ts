4cd9499f49c3a94d95afc1c7de781d5f7c1c8647
Wayne Van Son <waynevanson@gmail.com>
2021-08-14 10:15:36 +1000

    test(environment): add global driver property

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -2,4 +2,8 @@ describe("environment", () => {
   it("should have a browser object defined", () => {
     expect(browser).toBeDefined()
   })
+
+  it("should have a driver object defined", () => {
+    expect(driver).toBeDefined()
+  })
 })
