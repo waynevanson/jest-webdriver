638bab1184e80e35002799437882ed1f23d089e1
Wayne Van Son <waynevanson@gmail.com>
2021-08-14 10:08:16 +1000

    test(config): add headless chrome capabilities

--- configuration/jest.config.ts
+++ configuration/jest.config.ts
@@ -8,8 +8,16 @@ const configuration: InitialOptionsTsJest = {
   rootDir: "../",
   globals: {
     webdriverio: {
-      capabilities: {},
+      logLevel: "silent",
+      capabilities: {
+        // @ts-ignore this works
+        browserName: "chrome",
+        // @ts-ignore this works
+        "goog:chromeOptions": { headless: true },
+      },
+      services: [`chromedriver`],
     },
+    // ts-jest throws when globals is defined but there is not configuration
     "ts-jest": {},
   },
 }
