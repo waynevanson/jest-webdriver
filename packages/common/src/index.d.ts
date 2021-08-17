7a745933bbe79634a755ef8c7d7bed2358dc3ddd
Wayne Van Son <waynevanson@gmail.com>
2021-08-18 06:50:47 +1000

    refactor(common): extends ObjectConstructor assign signature

--- packages/common/src/index.d.ts
+++ packages/common/src/index.d.ts
@@ -1,10 +1,9 @@
-// Added so we can type check Jest types
-//
-// https://github.com/facebook/jest/issues/11640#issuecomment-893867514
-
 /// <reference types="node" />
 
 declare global {
+  // Added so we can type check Jest types
+  //
+  // https://github.com/facebook/jest/issues/11640#issuecomment-893867514
   namespace NodeJS {
     // eslint-disable-next-line
     interface Global {}
@@ -15,10 +14,21 @@ declare global {
     interface ConsoleConstructor extends console.ConsoleConstructor {}
   }
 
+  // strictly typed isArray
   interface ArrayConstructor {
     isArray<T>(
       value: T,
     ): value is Extract<T, Array<unknown> | ReadonlyArray<unknown>>
+  }
+
+  interface ObjectConstructor {
+    assign<T extends readonly Record<string, unknown>[]>(
+      ...args: T
+    ): T extends readonly (infer U)[]
+      ? U extends any
+        ? (r: U) => void extends (r: infer I) => void ? I : never
+        : never
+      : never
   }
 }
 
