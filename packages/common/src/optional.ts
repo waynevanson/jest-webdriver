c6aa8a1f4618ee6f53dc893bd9244def34d87051
Wayne Van Son <waynevanson@gmail.com>
2021-08-22 14:51:18 +1000

    feat(common): export partial from optional

--- packages/common/src/optional.ts
+++ packages/common/src/optional.ts
@@ -1,1 +1,20 @@
+import { option as O } from "fp-ts"
+import { pipe } from "fp-ts/lib/function"
+import * as o from "monocle-ts/Optional"
 export * from "monocle-ts/Optional"
+
+export const partial =
+  <A, P extends keyof A>(prop: P) =>
+  <S>(optional: o.Optional<S, A>): o.Optional<S, NonNullable<A[P]>> => ({
+    getOption: pipe(optional, o.prop(prop), o.fromNullable).getOption,
+    set: (b) => (s) =>
+      pipe(
+        optional.getOption(s),
+        O.fold(
+          () => ({} as A),
+          (a) => a,
+        ),
+        (a) => Object.assign({}, a, { [prop]: b }),
+        optional.set,
+      )(s),
+  })
