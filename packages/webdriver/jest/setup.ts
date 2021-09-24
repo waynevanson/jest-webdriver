9f310c6315efd424e559c0149a5383628716b7cd
Wayne Van Son <waynevanson@gmail.com>
2021-09-24 17:22:04 +1000

    refactor(webdriver): add type config to jest setup

--- packages/webdriver/jest/setup.ts
+++ packages/webdriver/jest/setup.ts
@@ -1,5 +1,7 @@
 import { setup } from "../../chromedriver/src"
+import { Config } from "@jest/types"
 
-export default async function () {
+// @todo –  use the global config option to get the options
+export default async function (_: Config.GlobalConfig) {
   await setup({ port: 4098, silent: true })
 }
