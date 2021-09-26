8a3b36a91eef534f8058b5a65179db9d527f94e5
Wayne Van Son <waynevanson@gmail.com>
2021-09-26 21:08:51 +1000

    fix(chromedriver): use cli props properly

--- packages/chromedriver/src/index.ts
+++ packages/chromedriver/src/index.ts
@@ -1,9 +1,19 @@
 import * as chromedriver from "chromedriver"
 import { options } from "./codecs"
 import * as c from "io-ts/Codec"
+import { either, readonlyArray } from "fp-ts"
+import * as d from "io-ts/Decoder"
+
 export async function setup(props: c.InputOf<typeof options>) {
-  // @ts-expect-error
-  await chromedriver.start(options.encode(props), true)
+  const result = options.decode(props)
+
+  if (either.isLeft(result)) {
+    throw new Error(d.draw(result.left))
+  }
+
+  const encoded = readonlyArray.compact(options.encode(result.right))
+
+  await chromedriver.start(encoded, true)
 }
 
 export async function teardown() {
