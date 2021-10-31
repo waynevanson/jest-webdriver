8207da92855b7fd62de0629b0cc8fea4f7e3bd07
waynevanson <waynevanson@gmail.com>
2021-10-31 11:06:32 +1100

    codecs

--- packages/chromedriver/src/index.ts
+++ packages/chromedriver/src/index.ts
@@ -1,93 +1,19 @@
-import {} from "chromedriver"
-import { option as O, readonlyArray as A } from "fp-ts"
-import { flow, pipe } from "fp-ts/lib/function"
+import { ChildProcess } from "child_process"
+import * as cd from "chromedriver"
+import { either as E } from "fp-ts"
+import { ChromeDriverOptions, decoder } from "./options"
 import * as d from "io-ts/Decoder"
 
-export type LogLevel = "ALL" | "DEBUG" | "INFO" | "WARNING" | "SEVERE" | "OFF"
+export function start(
+  chromeDriverOptions: ChromeDriverOptions
+): Promise<ChildProcess> {
+  const parsed = decoder.decode(chromeDriverOptions)
 
-export const silent: d.Decoder<
-  Partial<Record<"silent", true>>,
-  O.Option<string>
-> = pipe(
-  d.fromPartial({ silent: d.literal(true) }),
-  d.map(
-    flow(
-      O.fromNullableK(({ silent }) => silent),
-      O.map(() => "--silent")
-    )
-  )
-)
-
-export const verbose: d.Decoder<
-  Partial<Record<"verbose", true>>,
-  O.Option<string>
-> = pipe(
-  d.fromPartial({ verbose: d.literal(true) as d.Decoder<true, true> }),
-  d.map(
-    flow(
-      O.fromNullableK(({ verbose }) => verbose),
-      O.map(() => "--verbose")
-    )
-  )
-)
-
-export const logLevel: d.Decoder<
-  Record<"logLevel", LogLevel>,
-  O.Option<string>
-> = pipe(
-  d.fromPartial({
-    logLevel: d.literal("ALL", "DEBUG", "INFO", "WARNING", "SEVERE", "OFF"),
-  }),
-  d.map(
-    flow(
-      O.fromNullableK(({ logLevel }) => logLevel),
-      O.map((logLevel) => `--log-level="${logLevel}"`)
-    )
-  )
-)
-
-export const logging = d.union(silent, verbose, logLevel)
-
-export const port = pipe(
-  d.fromPartial({ port: d.id<number>() }),
-  d.map(O.fromNullableK(({ port }) => port)),
-  d.map(O.map((port) => `--port="${port}"`))
-)
-
-export const adbPort = pipe(
-  d.fromPartial({ adbPort: d.id<number>() }),
-  d.map(O.fromNullableK(({ adbPort }) => adbPort)),
-  d.map(O.map((adbPort) => `--adb-port="${adbPort}"`))
-)
-
-export const logPath = pipe(
-  d.fromPartial({ logPath: d.string }),
-  d.map(O.fromNullableK(({ logPath }) => logPath)),
-  d.map(O.map((logPath) => `--log-path="${logPath}"`))
-)
-
-export const options = pipe(
-  logging,
-  d.intersect(port),
-  d.intersect(adbPort),
-  d.intersect(logPath),
-  d.map(O.foldW(() => A.empty, A.of))
-)
-
-export interface ChromeDriverOptions {
-  port?: number
-  adbPort?: number
-  logPath?: string
-  logLevel?: LogLevel
-  verbose?: boolean
-  silent?: boolean
-  appendLog?: boolean
-  replayable?: boolean
-  version?: true
-  readableTimestamp?: boolean
-  urlBase?: string
-  enableChromeLogs?: boolean
-  disableDevShmUsage?: boolean
-  allowedIps?: Array<string>
-  allowedOrigin?: Array<string>
+  if (E.isLeft(parsed)) {
+    throw new Error(d.draw(parsed.left))
+  } else {
+    return cd.start(parsed.right, true)
+  }
 }
+
+export { stop } from "chromedriver"
---
+++ packages/chromedriver/src/options.ts
@@ -0,0 +1,160 @@
+import { option as O, readonlyArray as A } from "fp-ts"
+import { pipe } from "fp-ts/lib/function"
+import * as d from "io-ts/Decoder"
+
+export type LogLevel = "ALL" | "DEBUG" | "INFO" | "WARNING" | "SEVERE" | "OFF"
+
+export const asTrue: d.Decoder<true, true> = d.literal(true)
+export const asNumber: d.Decoder<number, number> = d.number
+export const asString: d.Decoder<string, string> = d.string
+
+export function fromPartialNullable<K extends string>(
+  property: K
+): <A>(
+  decoder: d.Decoder<A, A>
+) => d.Decoder<Partial<Record<K, A>>, O.Option<A>> {
+  return (decoder) =>
+    pipe(
+      d.fromPartial({ [property]: decoder }),
+      d.map(O.fromNullableK((partial) => partial[property]))
+    )
+}
+
+const fromPartialFlag = <K extends string>(property: K) =>
+  fromPartialNullable(property)(asTrue)
+
+export const silent: d.Decoder<
+  Partial<Record<"silent", true>>,
+  O.Option<string>
+> = pipe(fromPartialFlag("silent"), d.map(O.map(() => "--silent")))
+
+export const verbose: d.Decoder<
+  Partial<Record<"verbose", true>>,
+  O.Option<string>
+> = pipe(fromPartialFlag("verbose"), d.map(O.map(() => "--verbose")))
+
+export const logLevel: d.Decoder<
+  Record<"logLevel", LogLevel>,
+  O.Option<string>
+> = pipe(
+  d.literal("ALL", "DEBUG", "INFO", "WARNING", "SEVERE", "OFF"),
+  fromPartialNullable("logLevel"),
+  d.map(O.map((logLevel) => `--log-level="${logLevel}"`))
+)
+
+export const enableChromeLogs: d.Decoder<
+  Partial<Record<"enableChromeLogs", true>>,
+  O.Option<string>
+> = pipe(
+  fromPartialFlag("enableChromeLogs"),
+  d.map(O.map(() => `--enable-chrome-logs`))
+)
+
+export const port = pipe(
+  asNumber,
+  fromPartialNullable("port"),
+  d.map(O.map((port) => `--port="${port}"`))
+)
+
+export const adbPort = pipe(
+  asNumber,
+  fromPartialNullable("port"),
+  d.map(O.map((adbPort) => `--adb-port="${adbPort}"`))
+)
+
+export const logPath = pipe(
+  asString,
+  fromPartialNullable("logPath"),
+  d.map(O.map((logPath) => `--log-path="${logPath}"`))
+)
+
+// export const allowed = pipe(
+//   [
+//     ["allowedIps", "--allowed-ips"],
+//     ["allowedOrigins", "--allowed-origins"],
+//   ] as const,
+//   A.reduce(
+//     pipe(
+//       d.id<Partial<Record<"allowedOrigins" | "allowedIps", Array<string>>>>()
+//     ),
+//     (b, [property, flag]) =>
+//       pipe(
+//         d.fromArray(asString),
+//         fromPartialNullable(property),
+//         d.map(O.map((alloweds) => alloweds.join(","))),
+//         d.map(O.map((alloweds) => `${flag}="${alloweds}"`)),
+//         d.intersect(b)
+//       )
+//   )
+// )
+
+// const flags = pipe(
+//   [
+//     ["appendLog", "--append-log"],
+//     ["replayable", "--replayable"],
+//     ["readableTimestamp", "readable-timestampe"],
+//     ["disableDevShmUsage", "--disable-dev-shm-usage"],
+//   ] as const,
+//   A.reduce(
+//     d.id<
+//       Partial<
+//         Record<
+//           | "appendLog"
+//           | "replayable"
+//           | "readableTimestamp"
+//           | "disableDevShmUsage",
+//           true
+//         >
+//       >
+//     >(),
+//     (b, [property, flag]) =>
+//       pipe(
+//         fromPartialFlag(property),
+//         d.map(O.map(() => flag as string)),
+//         d.intersect(b)
+//       )
+//   )
+// )
+
+export const logging = d.union(silent, verbose, logLevel, enableChromeLogs)
+
+export const options = pipe(
+  logging,
+  d.intersect(port),
+  d.intersect(adbPort),
+  d.intersect(logPath)
+  // d.intersect(flags),
+  // d.intersect(allowed),
+)
+
+const version = pipe(
+  fromPartialFlag("version"),
+  d.map(O.map(() => `--version`))
+)
+
+export const decoder = pipe(
+  d.union(options, version),
+  d.map(O.fold(() => A.zero<string>(), A.of))
+)
+
+export type ChromeDriverOptions = d.InputOf<typeof options>
+
+export interface _ChromeDriverOptions {
+  port?: number
+  adbPort?: number
+  logPath?: string
+
+  enableChromeLogs?: boolean
+  logLevel?: LogLevel
+  verbose?: boolean
+  silent?: boolean
+
+  appendLog?: boolean
+  replayable?: boolean
+  version?: true
+  readableTimestamp?: boolean
+  disableDevShmUsage?: boolean
+  urlBase?: string
+  allowedIps?: Array<string>
+  allowedOrigin?: Array<string>
+}
